2a9905f226507c49be5e49bd62593bea556625c4
bartlomiej-gora <bartlomiej.gora@gmail.com>
2019-01-01 11:10:21 +0100

    Dodano podstawowe klasy

---
+++ src/main/java/com/github/bartlomiej/gora/bumps/BumpsTheRoad.java
@@ -0,0 +1,8 @@
+package com.github.bartlomiej.gora.bumps;
+
+class BumpsTheRoad {
+
+    public static String bumps(final String road) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/bartlomiej/gora/bumps/BumpsTheRoadTest.java
@@ -0,0 +1,10 @@
+package com.github.bartlomiej.gora.bumps;
+
+class BumpsTheRoadTest {
+
+
+    private BumpsTheRoad bumpsTheRoad;
+
+    ///@Test
+    //TODO: tutaj dodaj pierwszy test
+}
